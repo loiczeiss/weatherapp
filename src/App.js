f9ec8249a4a2732e6f116c805a0496e1d8a0ac5a
Loïc Zeiss <loiczeiss@gmail.com>
2022-11-18 13:51:15 +0100

    ternary in place, so what is it gonna be dante??

--- src/App.js
+++ src/App.js
@@ -1,65 +1,52 @@
-import {  useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
-import './App.css'
+import "./App.css";
 
 import Weather from "./Weather";
 import Search from "./Search";
 import WeatherS from "./Weather2";
+import { useEffect } from "react";
 
-
-function App({onSearchChange}) {
-
-
-
-// const [weatherBx, setWeatherBx] = useState({})
-
-//   useEffect(() => {
-//     axios.get('https://api.open-meteo.com/v1/forecast?latitude=50.8333&longitude=4.35&hourly=temperature_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone=auto')
-// .then((res) => res)
-// .then((res) => {
-//   setWeatherBx(res.data)
-//   console.log(weatherBx)
-//   })}, [])
-  
-
-  const [weatherFetcho, setWeatherfetcho] = useState({})
-  
-const HandleOnSearchChange = (searchData={
-  "value": "50.8465573 4.351697",
-  "label": "Bruxelles, Bruxelles-Capitale, Belgique"
-} ) => {
+function App({ onSearchChange }) {
+  const [weatherFetcho, setWeatherfetcho] = useState({});
+  const [weatherCode, setWeatherCode]= useState()
  
-  const [lat, long] = searchData.value.split (" ")
+
+console.log(weatherFetcho)
+  const HandleOnSearchChange = (
+    searchData = {
+      value: "50.8465573 4.351697",
+      label: "Bruxelles, Bruxelles-Capitale, Belgique",
+    }
+  ) => {
+    const [lat, long] = searchData.value.split(" ");
+
+    const fetchWeather = axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone=auto`
+    );
+    Promise.all([fetchWeather]).then(async (response) => {
+      const weatherResponse = await response[0].data;
+
+      setWeatherfetcho(weatherResponse);
+      setWeatherCode(weatherFetcho.current_weather && weatherFetcho.current_weather.weathercode)
   
-
-
-  const fetchWeather = axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&current_weather=true&timezone=auto`)
-Promise.all([fetchWeather ]).then(async (response) =>
-{
-  const weatherResponse = await response[0].data; 
-
-  setWeatherfetcho(weatherResponse)
-
-})
-
-
-
-}
-
-
+    });
+  };
+  console.log(weatherFetcho.current_weather.weathercode)
   return (
     <div
-    style={{background: "linear-gradient(to top, #fc4a1a, #f7b733)"}}
-     className="App h-screen	flex flex-col ">
-    
-
-<div className="div h-1/6 flex flex-col	 ">
-      <Weather onLoad={HandleOnSearchChange} weather={weatherFetcho} /></div>
+      style={{ 'backgroundColor': weatherFetcho.current_weather.weathercode === 3 ? 'blue' : 'red' }}
+      className="App h-screen	flex flex-col "
+    >
+      <div className="div h-1/6 flex flex-col	 ">
+        <Weather onLoad={HandleOnSearchChange} weather={weatherFetcho} />
+      </div>
       <div className=" h-1/8  flex place-content-center	mt-4  justify-center">
-      <div className="place-items-center		">
-      <Search  onSearchChange={HandleOnSearchChange}/>
-      </div></div>
-      <WeatherS weather={weatherFetcho}/>
+        <div className="place-items-center		">
+          <Search onSearchChange={HandleOnSearchChange} />
+        </div>
+      </div>
+      <WeatherS weather={weatherFetcho} />
     </div>
   );
 }
